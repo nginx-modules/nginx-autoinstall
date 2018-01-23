09bac46cb846c4ce025dc51e4142154a253817cf
Maksim Marchenko <8bite5d0@users.noreply.github.com>
2018-01-23 13:57:58 +0300

    Update nginx-autoinstall.sh

--- nginx-autoinstall.sh
+++ nginx-autoinstall.sh
@@ -118,7 +118,7 @@ case $OPTION in
 			unzip v${NPS_VER}.zip 2>> /tmp/nginx-autoinstall-error.log 1>> /tmp/nginx-autoinstall-output.log
 			rm v${NPS_VER}.zip
 			
-			cd incubator-pagespeed-ngx-{NPS_VER}
+			cd incubator-pagespeed-ngx-${NPS_VER}
 			psol_url=https://dl.google.com/dl/page-speed/psol/${NPS_VERSION}.tar.gz
 			[ -e scripts/format_binary_url.sh ] && psol_url=$(scripts/format_binary_url.sh PSOL_BINARY_URL)
 			wget ${psol_url} 2>> /tmp/nginx-autoinstall-error.log 1>> /tmp/nginx-autoinstall-output.log
